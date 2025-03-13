a378c03760a687c85f26c02f5ac4e8abd6629ed8
maxalanclark <maxaclark@gmail.com>
2025-03-13 12:38:51 -0700

    simulating most movs i think, thru listing 44

--- mc_86/Exec.cs
+++ mc_86/Exec.cs
@@ -3,30 +3,26 @@ using System;
 
 public static class Exec {
 
-    public static void MoveRmImm(mc_86.Reg dest, bool w, byte lo, byte hi) 
-    //public static void MoveRmImm(mc_86.Reg dest, bool w, byte hi, byte low) 
-    {
-        // i assume if w is false, we leave the high bits undisturbed?
-        if (w) dest.hi = hi;
-        dest.lo = lo;
-    }
-    
     public static void MoveRmRm(mc_86.Reg dest, mc_86.Reg src, bool w) {
         mc_86.debug("here we are now");
+        string cached = mc_86.ToHex(mc_86.ToInt16(dest.lo, dest.hi));
         if (w) // i'm assuming this is how we know to use 1 or 2 bytes 
         {
-           dest.hi = src.hi;
-           dest.lo = src.lo;
-           // idk if we're supposed to empty out src?
-           
+            dest.hi = src.hi;
+            dest.lo = src.lo;
+            // idk if we're supposed to empty out src?
         }
         else 
         {
+            if (dest.hi != 0) Console.WriteLine("are we supposed to not empty out the high bits? idk");
             dest.hi = 0;
             dest.lo = src.lo;
         }
         mc_86.debug("w: " + w);
         mc_86.debug(src.name + " src had " + src.lo);
         mc_86.debug(dest.name + " now has " + dest.lo);
+
+        string movedData = mc_86.ToHex(mc_86.ToInt16(dest.lo, dest.hi)); 
+        Console.WriteLine($"mov {dest.name}, {src.name} ; {dest.name}:{cached}->{movedData}");
     } 
 }
