2915b6adb2b8fa4a250aea41a514bdf3c6ac2ab3
maxalanclark <maxaclark@gmail.com>
2025-04-07 14:44:47 -0700

    - Wrote my own json parser for Pairs, heh

--- part2/HaversineCalc/Generator.cs
+++ part2/HaversineCalc/Generator.cs
@@ -29,13 +29,124 @@ public static class Generator
             double y1 = c.Item2 + ySpread * (rand.NextDouble() - .5);
             pairs[i] = new Pair(x0, y0, x1, y1);
         }
+
+        // computing haversine on generated pairs
+        double average = 0;
+        for (int i = 0; i < pairs.Length; i++)
+        {
+            Pair p = pairs[i];
+            average += HaversineFormula.Calc(p.x0, p.y0, p.x1, p.y1, HaversineCalc.R_EARTH);
+        }
+        if (pairs.Length > 0) average /= pairs.Length;
+        else Console.WriteLine("0 pairs"); 
+        Console.WriteLine("generated average distance: " + average);
             
         string json = JsonSerializer.Serialize(pairs, new JsonSerializerOptions { WriteIndented = true });
 
         System.IO.File.WriteAllText("pairs.json", json);
     }
 
-    public static Pair[] ParsePairs(string fileName)
+    public static void AssignVar(ref Pair pair, List<char> integerPart, List<char> fractionalPart, int varIndex) {
+        string s_value = "";
+        for (int i = 0; i < integerPart.Count; i++) {
+            s_value += integerPart[i];
+        }
+        s_value += ".";
+        for (int i = 0; i < fractionalPart.Count; i++) {
+            s_value += fractionalPart[i];
+        }
+        Double.TryParse(s_value, out double value); 
+
+        switch (varIndex) {
+            case 0:
+                pair.x0 = value;
+                break;
+            case 1:
+                pair.y0 = value;
+                break;
+            case 2:
+                pair.x1 = value;
+                break;
+            case 3:
+                pair.y1 = value;
+                break;
+            default:
+                Console.WriteLine("WARNING: unhandled pair var index: " + varIndex);
+                break;
+        }
+    }
+
+    public static Pair[] ParsePairs(string fileName) {
+
+        if (!File.Exists(fileName))
+        {
+            Console.WriteLine("can't find json file at " + fileName);
+            return new Pair[0];
+        }
+        
+        string json = File.ReadAllText(fileName);
+
+        Console.WriteLine("parsing...");
+
+        List<Pair> pairs = new List<Pair>();
+        Pair pair = new Pair();
+        int pairValueIndex = 0;
+        List<char> integerPart = new List<char>();
+        List<char> fractionalPart = new List<char>();
+        HashSet<char> digits = new HashSet<char>() {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
+        bool passedDecimal = false;
+        bool readingVarName = false;
+        for (int i = 0; i < json.Length; i++) {
+            if (!readingVarName && digits.Contains(json[i])) {
+                if (passedDecimal) {
+                    //Console.WriteLine("adding to fractional: " + json[i]);     
+                    fractionalPart.Add(json[i]);
+                }
+                else {
+                    //Console.WriteLine("adding to integer: " + json[i]);
+                    integerPart.Add(json[i]); 
+                }
+            }
+            else {
+                switch (json[i]) {
+                    case '"':
+                        readingVarName = !readingVarName;
+                        break;
+
+                    case '}':
+                        AssignVar(ref pair, integerPart, fractionalPart, pairValueIndex);
+                        integerPart.Clear();
+                        fractionalPart.Clear();
+                        pairs.Add(pair);
+
+                        //Console.WriteLine($"adding pair x0:{pair.x0}, y0:{pair.y0}, x1:{pair.x1}, y1:{pair.y1}");
+                        pair = new Pair();
+                        pairValueIndex = 0;
+                        passedDecimal = false;
+                        break;
+
+                    case ',':
+                        if (integerPart.Count > 0) {
+                            AssignVar(ref pair, integerPart, fractionalPart, pairValueIndex);
+                            integerPart.Clear();
+                            fractionalPart.Clear();
+                            pairValueIndex++;
+                            passedDecimal = false;
+                        }
+                        break;
+
+                    case '.':
+                        passedDecimal = true;
+                        break;
+                }
+            }
+        }
+
+
+        return pairs.ToArray();
+    }
+
+    public static Pair[] OldParsePairs(string fileName)
     {
 
         if (!File.Exists(fileName))
--- part2/HaversineCalc/Pair.cs
+++ part2/HaversineCalc/Pair.cs
@@ -7,6 +7,9 @@ public class Pair
     [JsonInclude] public double x1;
     [JsonInclude] public double y1;
     
+    public Pair() {
+    }
+
     public Pair(double x0, double y0, double x1, double y1)
     {
         this.x0 = x0;
